6c10843782157a1ad60951f1a8ed023f41c2f2ca
arslanyousafsdet <arslanyousafsdet@gmail.com>
2023-04-24 15:45:29 -0400

    the light features has beed added

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,11 @@ public class Main {
         x.size="124cc";
         x.weight="6554kg";
 
+
+        lights neon=new lights();
+        neon.color="red";
+        neon.size="15cm";
+        neon.intensity="5000lm";
+
     }
 }
